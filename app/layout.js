f8fbde203ff7565d30cf65941ede539b1c5d7d52
Ayush Gupta <ayushgupta01195@gmail.com>
2024-09-06 08:40:12 +0530

    add background to questions page

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-black`}>
+      <body className={`${inter.className} bg-black dark`}>
         <FloatingNavDemo />
         {children}
       </body>
